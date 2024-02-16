938103386edbdc0f36b4dc10321fd6f9941ec283
Olti Gjoni <oltigjoni@Oltis-MacBook-Air.local>
2024-02-16 14:16:16 -0500

    base code

---
+++ AcademiX/AcademiX/AcademiXApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcademiXApp.swift
+//  AcademiX
+//
+//  Created by Olti Gjoni on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct AcademiXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
